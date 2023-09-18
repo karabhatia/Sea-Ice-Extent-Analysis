42c6407a71fd2a3c6d690a4da0bead048fe05a71
Kara Bhatia <138478479+karabhatia@users.noreply.github.com>
2023-09-19 01:59:18 +0530

    Calculating Anomaly (month wise)
    The anomaly in sea ice extent value for every month of every year is calculated.

---
+++ Anomaly Calc.py
@@ -0,0 +1,35 @@
+import os
+import pandas as pd
+
+
+seaIce_df = pd.read_excel("C:\\Users\\KARA BHATIA\\AppData\\Local\\Programs\\Python\\Python310\\indianoceanavg.xlsx")
+
+list_month=[]
+for row in seaIce_df.itertuples(index=False):
+    list_month.append([row.Jan, row.Feb, row.Mar, row.Apr, row.May, row.June, row.July, row.Aug, row.Sep, row.Oct, row.Nov, row.Dec])
+
+#function for finding the average sea ice extent value for a month across various years
+def yrmonavg(mon):
+    sum=0
+    for i in list_month:
+        sum+=i[mon]
+    avg_yr = sum/43
+    return avg_yr
+
+
+#function to calculate the anomaly for a given month
+def anomcalc(mon):
+    anom_list=[]
+    for i in list_month:
+        anom=i[mon]-yrmonavg(mon)
+        anom_list.append(anom)
+    return anom_list
+
+yrlist=[]
+for yrr in range(1980,2023):
+        yrlist.append(yrr)
+
+#appending this data into excel
+df = pd.DataFrame ({'Years': yrlist, 'Jan': anomcalc(0), 'Feb': anomcalc(1), 'Mar': anomcalc(2), 'Apr': anomcalc(3), 'May': anomcalc(4),'June':anomcalc(5), 'July':anomcalc(6), 'Aug': anomcalc(7), 'Sep':anomcalc(8), 'Oct': anomcalc(9), 'Nov': anomcalc(10), 'Dec': anomcalc(11) })
+df.to_excel('indianoceananomaly.xlsx')
+
